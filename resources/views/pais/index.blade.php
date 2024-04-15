c0bc72c14dd65d111406f3ec7b4b5226609ce9cd
Sebastian Rodriguez Camilo <segarodriguezcamilo@gmail.com>
2024-04-15 16:55:29 -0500

    Paises

--- resources/views/pais/index.blade.php
+++ resources/views/pais/index.blade.php
@@ -1,22 +1,12 @@
-<!doctype html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Listado de Países') }}
+        </h2>
+    </x-slot>
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Listado de Países</title>
-</head>
-
-<body>
-
-    <div class="container">
-        <h1>Listado de Países</h1>
-        <a href="{{route('paises.create') }}" class="btn btn-success">Agregar</a>
+    <div class="container mx-auto py-6">
+        <a href="{{ route('paises.create') }}" class="btn btn-success mb-4">Agregar</a>
         <table class="table">
             <thead>
                 <tr>
@@ -33,7 +23,7 @@
                     <td>{{ $pais->pais_nomb }}</td>
                     <td>{{ $pais->pais_capi }}</td>
                     <td>
-                    <a href="{{ route('paises.edit', ['pais' => $pais->pais_codi]) }}" class="btn btn-info">Editar</a>
+                        <a href="{{ route('paises.edit', ['pais' => $pais->pais_codi]) }}" class="btn btn-info">Editar</a>
                         <form action="{{ route('paises.destroy', ['pais' => $pais->pais_codi]) }}" method="POST" style="display: inline-block">
                             @method('DELETE')
                             @csrf
@@ -45,17 +35,4 @@
             </tbody>
         </table>
     </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
-</body>
-
-</html>
+</x-app-layout>
--- resources/views/pais/new.blade.php
+++ resources/views/pais/new.blade.php
@@ -1,18 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Add País') }}
+        </h2>
+    </x-slot>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Add País</title>
-  </head>
-  <body>
-    <div class="container">
-        <h1>Add País</h1>
+    <div class="container mx-auto py-6">
         <form method="POST" action="{{ route('paises.store') }}">
             @csrf
             <div class="mb-3">
@@ -30,8 +23,4 @@
             </div>
         </form>
     </div>
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
-</html>
+</x-app-layout>
